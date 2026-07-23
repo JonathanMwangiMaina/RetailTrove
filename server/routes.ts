5a56d86a5c75a548b5aa2f2c2ec60fd9a7780465
Jonathan Maina <104943475+JonathanMwangiMaina@users.noreply.github.com>
2026-07-23 06:08:55 +0000

    chore: ESM rules

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express, Request, Response } from "express";
 import { createServer, type Server } from "http";
-import { storage } from "./storage";
-import { insertProductSchema } from "../shared/schema";
+import { storage } from "./storage.js";
+import { insertProductSchema } from "../shared/schema.js";
 import { z } from "zod";
 
 export async function registerRoutes(app: Express): Promise<Server> {
