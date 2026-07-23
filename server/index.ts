d233c0311b45656bb5b95ee3b9eb9f8f48409eab
Jonathan Maina <104943475+JonathanMwangiMaina@users.noreply.github.com>
2026-07-23 18:07:17 +0000

    Update index.ts dynamic port assignment

--- server/index.ts
+++ server/index.ts
@@ -66,7 +66,7 @@ export default app;
 // ── Listen Only in Local Development ───────────────────────────────────────
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
 }
